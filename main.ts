6a9b657aa311310b02829706e5fb805017e2ded8
cdelcan812 <154432673+cdelcan812@users.noreply.github.com>
2024-04-10 13:19:08 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 10) {
         maqueen.motorStop(maqueen.Motors.M1)
     } else {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
     }
     basic.showNumber(maqueen.Ultrasonic(PingUnit.Centimeters))
 })
